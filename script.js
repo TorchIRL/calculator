7c00a5fc209a6a77b9dc73518380e1b187d720af
TorchIRL <156404688+TorchIRL@users.noreply.github.com>
2024-02-04 19:05:54 +1000

    Updated files for desktop

---
+++ script.js
@@ -0,0 +1,9 @@
+const shell = document.querySelector('.calcShell')
+const buttons = document.querySelector('.buttons')
+
+const minus = document.querySelector('#minus')
+minus.textContent = '-'
+const oBracket = document.querySelector('#oBracket')
+oBracket.textContent = '('
+const cBracket = document.querySelector('#cBracket')
+cBracket.textContent = ')'
