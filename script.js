5ad217fce3bc7cfe72c0c9f6b92dd63309e6868a
TorchIRL <156404688+TorchIRL@users.noreply.github.com>
2024-02-05 19:47:22 +1000

    Prototype Calculator Works calculator roughly works for single digit numbers as they on screen with accurate calculations, double digit numbers to be implemented

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const minus = document.querySelector('#minus')
 minus.textContent = '-'
 const oBracket = document.querySelector('#oBracket')
@@ -14,39 +12,36 @@ let num1 = 0;
 let num2 = 0;
 let operator = '';
 let result = 0;
-function add(num1, num2){
-    let a = num1 + num2;
-    return a
+
+function add() {
+    result = num1 + num2;
+    return result
 }
 
-function subtract(num1, num2){
-    let s = num1 - num2;
-    return s
+function subtract(){
+    result = num1 - num2;
+    return result
 }
 
-function divide(num1, num2){
-    let d = num1 / num2;
-    return d
+function divide(){
+    result = num1 / num2;
+    return result
 }
 
-function multiply(num1, num2){
+function multiply(){
     result = num1 * num2;
     return result
 }
 
-function operate(num1, operator, num2){
+function operate(){
     if (operator === '+') {
-        add(num1, num2)
-        document.getElementById('display').value = result;
+        add()
     } else if (operator === '-') {
-        subtract(num1, num2)
-        document.getElementById('display').value = s;
+        subtract()
     } else if (operator === '/') {
-        divide(num1, num2)
-        document.getElementById('display').value = d;
+        divide()
     } else if (operator === 'x') {
-        multiply(num1, num2);
-        document.getElementById('display').value = m;
+        multiply();
     }
 } 
 buttons.addEventListener('click', (event) => {
@@ -54,64 +49,110 @@ buttons.addEventListener('click', (event) => {
 
     switch(target.id) {
         case 'oBracket':
-            document.getElementById('display').value += '(';
+            document.getElementById('display').value = '(';
             break;
         case 'cBracket':
-            document.getElementById('display').value += ')';
+            document.getElementById('display').value = ')';
             break;
         case 'percent':
-            document.getElementById('display').value += '%';
+            document.getElementById('display').value = '%';
             break;
         case 'reset':
             document.getElementById('display').value = '';
+            num1 = 0
+            num2 = 0
+            operator = ''
             break;
         case 'one':
-            document.getElementById('display').value += '1';
+            if (operator === '') {
+                document.getElementById('display').value = `${num1 = 1}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value = `${num1 = 1}`;
+            }
             break;
         case 'two':
-            document.getElementById('display').value += '2';
-            break;        
+            if (operator === '') {
+                document.getElementById('display').value = `${num1 = 2}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value = `${num2 = 2}`;
+            }
+            break;  
         case 'three':
-            document.getElementById('display').value += '3';
-            num2 = 3
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 3}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 3}`;
+            }
             break;
         case 'four':
-            document.getElementById('display').value += '4';
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 4}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 4}`;
+            }
             break;
         case 'five':
-            document.getElementById('display').value += '5';
-            num1 = 5
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 5}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 5}`;
+            }
             break;
         case 'six':
-            document.getElementById('display').value += '6';
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 6}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 6}`;
+            }
             break;
         case 'seven':
-            document.getElementById('display').value += '7';
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 7}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 7}`;
+            }
             break;
         case 'eight':
-            document.getElementById('display').value += '8';
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 8}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 8}`;
+            }
             break;
         case 'nine':
-            document.getElementById('display').value += '9';
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 9}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 9}`;
+            }
             break;
         case 'zero':
-            document.getElementById('display').value += '0';
+            if (operator === '') {
+                document.getElementById('display').value += `${num1 = 0}`;
+            } else if (operator !== '') {
+                document.getElementById('display').value += `${num2 = 0}`;
+            }
             break;
         case 'plus':
-            document.getElementById('display').value += '+';
-            operator = '+'
+            document.getElementById('display').value = '+';
+            operator = '+';
             break;
         case 'equal':
-            document.getElementById('display').value = operate(num1, num2, operator);
+            document.getElementById('display').value = operate();
+            let str = result.toString()
+            document.getElementById('display').value = str;
             break;
         case 'minus':
-            document.getElementById('display').value += '-';
+            document.getElementById('display').value = '-';
+            operator = '-';
             break;
         case 'multiply':
-            document.getElementById('display').value += 'x';
+            document.getElementById('display').value = 'x';
+            operator = 'x';
             break;
         case 'divide':
-            document.getElementById('display').value += '÷';
+            document.getElementById('display').value = '÷';
+            operator = '/';
             break;
         case 'decimal':
             document.getElementById('display').value += '.';
